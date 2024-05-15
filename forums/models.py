f8a821c422639f6459387b7b6af439aa7917f56d
Margaux Boileau <margaux.boileau.7e6@itb.cat>
2024-05-15 09:30:13 +0200

    permissions

--- forums/models.py
+++ forums/models.py
@@ -5,6 +5,6 @@ class Forum(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     date = models.DateField(default=datetime.date.today)
-    read_members = models.ManyToManyField('shops.Shop', related_name='read_members')
-    read_write_members = models.ManyToManyField('shops.Shop', related_name='read_write_members')
+    read_members = models.ManyToManyField('shops.Shop', related_name='read_members', blank=True)
+    read_write_members = models.ManyToManyField('shops.Shop', related_name='read_write_members', blank=True)
     posts = models.ManyToManyField('posts.Post', blank=True)
--- posts/views.py
+++ posts/views.py
@@ -18,18 +18,11 @@ from rest_framework.permissions import IsAuthenticated
 from fcm_django.models import FCMDevice
 from firebase_admin.messaging import Message, Notification
 
-# /forums/<forum_pk>/posts/
+# URL: /forums/<forum_pk>/posts/
+class PostListView(APIView):    
+    permission_classes = [IsAuthenticated]
 
-class PostListView(APIView):
-    def dispatch(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            self.permission_classes = [IsAuthenticated]
-        else:
-            self.permission_classes = []
-
-        return super().dispatch(request, *args, **kwargs)
-    
-    # PERMISSIONS: read-members of forum
+    # PERMISSIONS: read members of forum
     def get(self, request, forum_pk):
         itemsPerPage = self.request.query_params.get('itemsPerPage')
         page = self.request.query_params.get('page')
@@ -40,13 +33,19 @@ class PostListView(APIView):
         except Forum.DoesNotExist:
             return Response({"error": "Forum does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
+        user = request.user
+        if not user.shop_working_at:
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+        elif not forum.read_members.filter(id=user.shop_working_at.id).exists():
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+
         posts = Post.objects.filter(forum=forum).order_by('-id')
         pagination = PageNumberPagination()
         page = pagination.paginate_queryset(posts, request)
         serializer = PostSerializer(page, many=True)
         return pagination.get_paginated_response(serializer.data)
     
-    # PERMISIIONS: write members
+    # PERMISIIONS: write members of forum
     def post(self, request, forum_pk):
         try:
             forum = Forum.objects.get(pk=forum_pk)
@@ -54,7 +53,9 @@ class PostListView(APIView):
             return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
         user = request.user
-        if not forum.read_write_members.filter(id=user.shop_working_at.id).exists():
+        if not user.shop_working_at:
+            return Response({"error": "You do not have permission to post in this forum."}, status=status.HTTP_403_FORBIDDEN)
+        elif not forum.read_write_members.filter(id=user.shop_working_at.id).exists():
             return Response({"error": "You do not have permission to post in this forum."}, status=status.HTTP_403_FORBIDDEN)
 
         data = request.data
@@ -68,7 +69,6 @@ class PostListView(APIView):
         
         media_objects = []
         for media in medias:
-            #print(media)
             decoded = base64.b64decode(media)
 
             media_name = f'{uuid.uuid4()}.jpg'
@@ -90,35 +90,78 @@ class PostListView(APIView):
                 )
                 device.send_message(message)
 
-        return Response(status=status.HTTP_201_CREATED)
+        return Response({"message": "Post created successfully."}, status=status.HTTP_201_CREATED)
 
-# /forums/<forum_pk>/posts/<post_pk>
+# URL: /forums/<forum_pk>/posts/<post_pk>/
 class PostDetailView(APIView):
-    # PERMISSIONS: read-members of forum
+    permission_classes = [IsAuthenticated]
+
+    # PERMISSIONS: read members of forum
     def get(self, request, forum_pk, post_pk):
+        try:
+            forum = Forum.objects.get(pk=forum_pk)
+        except ObjectDoesNotExist:
+            return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
+                  
         try:
             post = Post.objects.get(pk=post_pk)
         except Post.DoesNotExist:
             return Response({"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = PostSerializer(post)
         
-        return Response(serializer.data)
+        user = request.user
+        if not user.shop_working_at:
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+        elif not forum.read_members.filter(id=user.shop_working_at.id).exists():
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
 
-# /forums/<forum_pk>/posts/<post_pk>/comments/
+        serializer = PostSerializer(post)        
+        return Response(serializer.data)
+    
+    # PERMISSIONS: creator of post
+    def delete(self, request, forum_pk, post_pk):
+        try:
+            forum = Forum.objects.get(pk=forum_pk)
+        except ObjectDoesNotExist:
+            return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
+                  
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        
+        user = request.user
+        if not user.shop_working_at:
+            return Response({"error": "You do not have permission to delete this post."}, status=status.HTTP_403_FORBIDDEN)
+        elif not user.shop_working_at==post.id_creator:
+            return Response({"error": "You do not have permission to delete this post."}, status=status.HTTP_403_FORBIDDEN)
+        
+        post.delete()
+        return Response({"message": "Post deleted successfully from the forum."}, status=status.HTTP_200_OK)
+
+# URL: /forums/<forum_pk>/posts/<post_pk>/comments/
 class CommentsListView(APIView):
     permission_classes = [IsAuthenticated]
 
-    # PERMISSIONS: read-members of forum
+    # PERMISSIONS: read members of forum
     def get(self, request, forum_pk, post_pk):
         try:
             forum = Forum.objects.get(pk=forum_pk)
-            post = Post.objects.get(pk=post_pk)
         except ObjectDoesNotExist:
-            return Response({"error": "Object does not exist."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
+                  
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
         
         if post not in forum.posts.all():
             return Response({"error": "Post does not belong to this forum."}, status=status.HTTP_404_NOT_FOUND)
+
+        user = request.user
+        if not user.shop_working_at:
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+        elif not forum.read_members.filter(id=user.shop_working_at.id).exists():
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
 
         comments = post.comments.all().order_by('-id')
         pagination = PageNumberPagination()
@@ -126,71 +169,87 @@ class CommentsListView(APIView):
         serializer = CommentSerializer(page, many=True)
         return pagination.get_paginated_response(serializer.data)
     
-    # PERMISSIONS: read-members of forum
+    # PERMISSIONS: read members of forum
     def post(self, request, forum_pk, post_pk):
-        user = request.user
-        data = request.data
-
         try:
             forum = Forum.objects.get(pk=forum_pk)
-            post = Post.objects.get(pk=post_pk)
         except ObjectDoesNotExist:
-            return Response({"error": "Object does not exist."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
+                  
+        try:
+            post = Post.objects.get(pk=post_pk)
+        except Post.DoesNotExist:
+            return Response({"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
         
-        if user.shop_working_at not in forum.read_members.all():
-            return Response({"error": "You do not have permission."}, status=status.HTTP_403_FORBIDDEN)
-
         if post not in forum.posts.all():
             return Response({"error": "Post does not belong to this forum."}, status=status.HTTP_404_NOT_FOUND)
+
+        user = request.user
+        if not user.shop_working_at:
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+        elif not forum.read_members.filter(id=user.shop_working_at.id).exists():
+            return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
         
         try:
             comment = Comment.objects.create(
                 creator=user.shop_working_at,
-                content=data.get('content'))
+                content=request.data.get('content'))
             post.comments.add(comment)
             return Response({"message": "Comment created sucessfully"} ,status=status.HTTP_201_CREATED)
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_400_BAD_REQUEST)    
 
+# URL: /comments/<pk_comment>/
 # PERMISSIONS: owner of comment
-# /comments/<pk_comment/
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated, ])
-def delete_comment(request, comment_pk):
-    user = request.user
-    
+def delete_comment(request, comment_pk): 
     try:
         comment = Comment.objects.get(pk=comment_pk)
     except ObjectDoesNotExist:
         return Response({"error": "Comment does not exist."}, status=status.HTTP_404_NOT_FOUND)
     
-    if user.shop_working_at != comment.creator:
-        return Response({"error": "You do not have permission."}, status=status.HTTP_403_FORBIDDEN)
+    user = request.user
+    if not user.shop_working_at:
+        return Response({"error": "You do not have permission to delete this comment."}, status=status.HTTP_403_FORBIDDEN)
+    elif user.shop_working_at != comment.creator:
+        return Response({"error": "You do not have permission to delete this comment."}, status=status.HTTP_403_FORBIDDEN)
     
     comment.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response({"message": "Comment deleted successfully."}, status=status.HTTP_200_OK)
 
-# PERMISSIONS: read-members of forum
-# /forums/<forum_pk>/posts/<post_pk>/media/<media_pk/
+# URL: /forums/<forum_pk>/posts/<post_pk>/media/<media_pk/
+# PERMISSIONS: read members of forum
 @api_view(['GET'])
 def get_post_media(request, forum_pk, post_pk, media_pk):
     try:
         forum = Forum.objects.get(pk=forum_pk)
-        post = Post.objects.get(pk=post_pk)
-        media = Media.objects.get(pk=media_pk)
     except ObjectDoesNotExist:
-        return Response({"error": "Object does not exist."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Forum does not exist."}, status=status.HTTP_404_NOT_FOUND)
+              
+    try:
+        post = Post.objects.get(pk=post_pk)
+    except Post.DoesNotExist:
+        return Response({"error": "Post does not exist"}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        media = Media.objects.get(pk=media_pk)
+    except Media.DoesNotExist:
+        return Response({"error": "Media does not exist"}, status=status.HTTP_404_NOT_FOUND)
     
     if post not in forum.posts.all():
         return Response({"error": "Post does not belong to this forum."}, status=status.HTTP_404_NOT_FOUND)
     
+    user = request.user
+    if not user.shop_working_at:
+        return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+    elif not forum.read_members.filter(id=user.shop_working_at.id).exists():
+        return Response({"error": "You do not have access to this forum."}, status=status.HTTP_403_FORBIDDEN)
+
     if media not in post.media.all():
         return Response({"error": "Media does not belong to this post."}, status=status.HTTP_404_NOT_FOUND)
     
     media_name = media.id
-    print(media_name)
     img = open('uploads/'+media_name, 'rb')
-
     response = FileResponse(img)
-
-    return response    
+    return response 
